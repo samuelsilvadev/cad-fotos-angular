6db90c408f4158397a84e9c97b90a24363fdc027
Samuel Silva <samuelsilvawb@gmail.com>
2017-11-21 14:09:48 -0300

    feat : Criar navegabilidade entre páginas
    - Criado botão para cadastrar as fotos
    - Criado botão para voltar para home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,8 @@ import { CadastroFotoComponent } from './cadastro-foto/cadastro-foto.component';
 const APP_ROUTES: Routes = [
   { path: '', component: ListagemFotoComponent },
   { path: 'cadastro', component: CadastroFotoComponent },
-  { path: 'lista', component: ListagemFotoComponent }
+  { path: 'lista', component: ListagemFotoComponent },
+  { path: '**', component: ListagemFotoComponent }
 ];
 
 export const ROUTING = RouterModule.forRoot(APP_ROUTES);
