c4eab5bea048e3d847fc6bab1cd7d50422c97ae8
Samuel Silva <samuelsilvawb@gmail.com>
2017-11-21 12:55:54 -0300

    feat : Criar arquivo de rotas
    - Criado arquivo de rotas
    - Importado no modulo principal

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListagemFotoComponent } from './listagem-foto/listagem-foto.component';
+import { CadastroFotoComponent } from './cadastro-foto/cadastro-foto.component';
+
+
+
+const APP_ROUTES: Routes = [
+  { path: '', component: ListagemFotoComponent },
+  { path: 'cadastro', component: CadastroFotoComponent },
+  { path: 'lista', component: ListagemFotoComponent }
+];
+
+export const ROUTING = RouterModule.forRoot(APP_ROUTES);
